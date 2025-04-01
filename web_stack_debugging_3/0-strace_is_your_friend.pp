fccd16eb3c38c14fb6b3f54d8528653edeb32da2
codestantceasar <c.akas@alustudent.com>
2025-04-01 05:54:22 +0200

    add this file

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# This Puppet script fixes the issue causing Apache to return a 500 error after debugging with strace.
 exec { 'fix-apache-error':
   command => 'chown -R www-data:www-data /var/www/html && systemctl restart apache2',
   path    => ['/bin', '/usr/bin'],
